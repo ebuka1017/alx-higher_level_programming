cd21bdc36126fc2027bd31f848862073e7bf8098
ebuka1017 <isaacokwuzi@gmail.com>
2023-08-10 20:01:35 -0700

    task 0 fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import add_0
 
-a = 1
-b = 2
-sUm = add_0.add(a, b)
+if __name__ == "__main__"
+    import add_0
 
-print("{} + {} = {}".format(a, b, sUm))
+    a = 1
+    b = 2
+    sUm = add_0.add(a, b)
+
+    print("{} + {} = {}".format(a, b, sUm))
