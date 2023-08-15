8ca42ca476dc0d6ca1d9fea025ab7b882942d0d5
ebuka1017 <isaacokwuzi@gmail.com>
2023-08-14 17:24:10 -0700

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0]
+
+    if (length == 0):
+        return ("None")
+    else:
+        return length, first
