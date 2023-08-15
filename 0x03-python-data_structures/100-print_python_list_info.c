58f6a3e347851d923637ac394840ed1262ea343a
ebuka1017 <isaacokwuzi@gmail.com>
2023-08-14 18:58:44 -0700

    task 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints some basic info about python lists
+ * @p: list object
+ *
+ * Return: nothing
+ */
+
+void print_python_list_info(PyObject *p)
+{
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t refcount = Py_REFCNT(p);
+
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", refcount);
+
+    for (Py_ssize_t i = 0; i < size; i++)
+    {
+        PyObject *item = PyList_GetItem(p, i);
+        const char *type = Py_TYPE(item)->tp_name;
+
+        printf("Element %ld: %s\n", i, type);
+    }
+}
+
