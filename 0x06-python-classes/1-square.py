f57c1056bcb279a9f2496700159c3e55f88f252b
ebuka1017 <isaacokwuzi@gmail.com>
2023-08-30 15:26:05 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2
+"""
+Square module
+"""
+
+
+class Square:
+    """creates a square class"""
+    def __init__(self, size):
+        """initialize a new square.
+
+        Args:
+            size: size of new square
+        """
+        self.__size = size
+
+    def getsize(self):
+        return self.__size
