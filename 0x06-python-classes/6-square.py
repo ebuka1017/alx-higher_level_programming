b2f67570c87fb9fe6d3042a648dc7e4372b5c7a4
ebuka1017 <isaacokwuzi@gmail.com>
2023-08-30 19:56:44 -0700

    task 6 fix: fixed typo

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -66,4 +66,4 @@ class Square:
             for _ in range(self.__position[1]):
                 print()
             for _ in range(self.__size):
-                print("{} ".format(" " * self.__position[0] + '#' * self.__size))
+                print("{}".format(" " * self.__position[0] + '#' * self.__size))
