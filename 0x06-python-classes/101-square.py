ba72a5e6137b3335f52a8bece100d1a1ac7e1650
ebuka1017 <isaacokwuzi@gmail.com>
2023-08-30 21:30:00 -0700

    task 101

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+"""Define a square class."""
+
+
+class Square:
+    """Creates a square class object."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        """
+        Initializes a square object with parameters.
+        Size:
+
+        Size (int) = size of the square
+        position (tuple) = represents new position
+        """
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """function returns the current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """gets size"""
+        return self.__size
+
+    @property
+    def position(self):
+        """gets position"""
+        return self.__position
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        """sets the size"""
+        self.__size = size
+
+    @position.setter
+    def position(self, value):
+        """sets the position"""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        x, y = value
+
+        if not all(isinstance(val, int) and val >= 0 for val in (x, y)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """prints square to stdout"""
+
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                if self.__position[0] > 0:
+                    print(" " * self.__position[0] + '#' * self.__size)
+                else:
+                    print("{}".format('#' * self.__size))
+
+    def __str__(self):
+        if(self.size == 0):
+            return ''
+        newlines = '\n' * self.position[1]
+        spaces = ' ' * self.position[0]
+        hashes = "#" * self.size
+        return newlines + '\n'.join(spaces + hashes for x in range(self.size))
