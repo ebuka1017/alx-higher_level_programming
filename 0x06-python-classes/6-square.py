79c08f0c9e6216ca55765cf0450fb353a010adbc
ebuka1017 <isaacokwuzi@gmail.com>
2023-08-30 20:18:29 -0700

    space err

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -66,4 +66,7 @@ class Square:
             for _ in range(self.__position[1]):
                 print()
             for _ in range(self.__size):
-                print(" " * self.__position[0] + '#' * self.__size)
+                if self.__position[0] > 0:
+                    print(" " * self.__position[0] + '#' * self.__size)
+                else:
+                    print("{}".format('#' * self.__size))
