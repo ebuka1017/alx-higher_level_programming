d5fbe9e19552200e99de4e1347d279f3e02327cf
ebuka1017 <isaacokwuzi@gmail.com>
2023-09-06 18:07:03 -0700

    task 1 fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,13 +15,18 @@ class Rectangle:
             width (int): width of the rectangle
             height (int): height of the rectangle
         """
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
         """returns width"""
         return self.__width
+
+    @property
+    def height(self):
+        """returns height"""
+        return self.__height
 
     @width.setter
     def width(self, value):
@@ -31,11 +36,6 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         """sets the width"""
         self.__width = value
-
-    @property
-    def height(self):
-        """returns height"""
-        return self.__height
 
     @height.setter
     def height(self, value):
